4b197910f049e851f52571bb7960aea13e1c0a16
Mauro Arantes <mauro.arantes@gmail.com>
2022-02-04 09:54:38 +0000

    Initial Commit

---
+++ PreStart1/PreStart1App.swift
@@ -0,0 +1,17 @@
+//
+//  PreStart1App.swift
+//  PreStart1
+//
+//  Created by Mauro Arantes on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PreStart1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
